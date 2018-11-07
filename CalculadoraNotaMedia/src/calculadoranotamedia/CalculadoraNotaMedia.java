9963b9c78dc850eb7eca00515ad56d6307fd1a92
lvazquezdorna <lvazquezdorna@nautilus17.danielcastelao.org>
2018-11-07 12:38:31 +0100

    terminado falta revisar

---
+++ CalculadoraNotaMedia/src/calculadoranotamedia/CalculadoraNotaMedia.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadoranotamedia;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Lara Vazquez Dorna
+ */
+public class CalculadoraNotaMedia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // istanciar objeto
+        Notas notas = new Notas();
+        int opcion;
+
+        System.out.println(
+                "************************************************************\n"
+                + "****\t\tBienvenido al calculador de Notas\t****"
+                + "\n************************************************************\n");
+
+        do {
+            opcion = menu();
+            switch (opcion) {
+                case 1:
+                    //nota media de la teoria
+                    notas.calcularNotaTeoria();
+                    break;
+                case 2:
+                    //nota media de la practica
+                    notas.calcularNotaPractica();
+                    break;
+                case 3:
+                    //nota boletines
+                    notas.calcularNotaBoletines();
+                    break;
+                case 4:
+                    //nota final
+                    notas.calcularNotaFinal();
+                    break;
+                case 0:
+                    System.out.println("\n\t Gracias por usar la calculadora.");
+                    break;
+                default:
+                    System.out.println("La operación elegida no existe.");
+                    break;
+            }
+        } while (opcion != 0);
+
+    }
+
+    private static int menu() {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("\nQue operación desea realizar:"
+                + "\n1=>Calcular nota teorica"
+                + "\n2=>Calcular nota practica"
+                + "\n3=>Calcular nota boletines"
+                + "\n4=>Calcular nota media"
+                + "\n0=>Salir");
+
+        return Integer.parseInt(teclado.nextLine());
+    }
+
+}
