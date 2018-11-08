e7a9e24d5127ac5140898749fde12d44eef06018
lvazquezdorna <lvazquezdorna@nautilus17.danielcastelao.org>
2018-11-08 09:09:25 +0100

    creada la clase experimental

---
+++ CalculadoraNotaMedia/src/calculadoranotamedia/Experimental.java
@@ -0,0 +1,12 @@
+/*
+ * esta clase es una prueba de branch para contorno de desenvolvimiento 
+ */
+package calculadoranotamedia;
+
+/**
+ *
+ * @author lara vazquez dorna
+ */
+public class Experimental {
+    
+}
